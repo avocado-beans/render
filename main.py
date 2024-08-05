63ad44682dbbab304d5274e8e8d9859cacb38081
anon <122219518+avocado-beans@users.noreply.github.com>
2024-08-05 11:31:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ import threading
 start = time.time()
 async def func():
     bot = telegram.Bot(os.environ['APIKEY'])
+    the_killers_monologue = "i serve no god, or country./ i fly no flag./ if i'm effective, it's because of one simple reason./ i./ don't/ give./ a./ fuck."
+    for i in the_killers_monologue.split('/'):
+        await bot.sendMessage(chat_id='@th3k1ll3r', text="i)
+        time.sleep(2)
     async with bot:
         while True:
             await bot.sendMessage(chat_id='@th3k1ll3r', text=f'~{round((time.time()-start)/60)} minutes have passed since i have been alive')
