01e3e750916aeecc95bea48fcebbe9b91a253835
anon <122219518+avocado-beans@users.noreply.github.com>
2024-08-05 09:42:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,24 +2,26 @@ from fastapi import FastAPI, Request
 import gemini
 import asyncio
 import base64
+import telegram
+import time
+import threading
+
+start = time.time()
+async def func():
+    bot = telegram.Bot("7296494753:AAFHYL8LtoGNHyWZs3yiyQV0S3gYrR5YhHA")
+    async with bot:
+        while True:
+            time.sleep(60)
+            await bot.sendMessage(chat_id='@thek1ll3r', text=f'{round(time.time()-start)/60} minutes have passed since i have been alive')
+
+def main():
+    asyncio.run(func())
+
+thread = threading.Thread(target=main,)
+thread.start()
 app = FastAPI()
 
 
 @app.get("/")
 async def confirm(request: Request):
     return "Your server is working!"
-    
-@app.post("/image/")
-async def read_image(request: Request):
-    request = await request.json()
-    with open("question.png", "wb") as image:
-        image.write(base64.b64decode(request['image'].replace('data:image/png;base64,','')))
-        
-    return gemini.text_in_image(request['key'], request['prompt'], "question.png", request['model_name'])
-
-@app.post("/text/")
-async def read_text(request: Request):
-    request = await request.json()
-    return gemini.text_only(request['key'], request['prompt'], request['model_name'])
-
-
