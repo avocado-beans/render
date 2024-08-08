01a5d92c37e30388f8752b86d5add88bd233e9de
anon <122219518+avocado-beans@users.noreply.github.com>
2024-08-08 14:50:04 +0200

    🚨

--- main.py
+++ main.py
@@ -188,6 +188,8 @@ def get_contract_wallet_txns(token_address, latest_block, back_stretch):
 
 async def func():
     bot = telegram.Bot(os.environ['TELEBOTAPI'])
+    await bot.sendMessage(chat_id='@th3k1ll3r', text=f"code base changed\nrebooted to apply updates")
+    time.sleep(10)
     await bot.sendMessage(chat_id='@th3k1ll3r', text=f"target chain: {chain.lower()}")
     time.sleep(5)
     await bot.sendMessage(chat_id='@th3k1ll3r', text="bravo6\ngoing dark")
