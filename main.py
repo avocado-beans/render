fe134bc07f78c6469e04148193b6dfaa96816b54
anon <122219518+avocado-beans@users.noreply.github.com>
2024-08-11 06:22:23 +0200

    trivial?

--- main.py
+++ main.py
@@ -34,6 +34,135 @@ width = minutes*blockspermin
 back_stretch = back_stretch_minutes*blockspermin
 front_limit = back_stretch - width
 
+async def func():
+    global w3
+    global TRANSFER_EVENT_SIGNATURE
+    w3 = Web3(Web3.HTTPProvider(provider_url))
+    print(w3.is_connected())
+    TRANSFER_EVENT_SIGNATURE = w3.keccak(text='Transfer(address,address,uint256)').hex()
+
+    bot = telegram.Bot(os.environ['TELEBOTAPI'])
+    await bot.sendMessage(chat_id='@th3k1ll3r', text=f"code base modified\nrebooted to apply updates")
+    time.sleep(10)
+    await bot.sendMessage(chat_id='@th3k1ll3r', text=f"target chain: {chain.lower()}")
+    time.sleep(2)
+    await bot.sendMessage(chat_id='@th3k1ll3r', text="bravo6\ngoing dark")
+    print("bravo6\ngoing dark")
+
+    while True:
+        p_start = time.time()
+        latest_block = w3.eth.block_number
+
+        print('\n***************************************')
+        print(f'checking what happened (~{back_stretch/(60*blockspermin)}hrs) ago on {chain.upper()}...')
+        no_of_chunks = round(minutes/5)
+
+        print(f'took {latest_block} as latest block')
+        hit_wall = False
+        for i in range(no_of_chunks):
+            if back_stretch - width*(i+1) < front_limit:
+                print('hit a wall')
+                hit_wall = True
+                break
+            print(f'parsing chunk {i+1}: { latest_block - (back_stretch - width*i)} to { latest_block - (back_stretch - width*(i+1))}')
+            filter_params = {
+                    'fromBlock': latest_block - (back_stretch - width*i),
+                    'toBlock': latest_block - (back_stretch - width*(i+1)),
+                    'topics': [TRANSFER_EVENT_SIGNATURE]
+                }
+            logs = w3.eth.get_logs(filter_params)
+            print('finished parsing chunk')
+            if i == 0:
+                t_logs = logs
+            elif i > 0:
+                t_logs += logs
+        if hit_wall:
+            time.sleep(60)
+            continue
+        creations = {}
+        print(f'Done parsing logs ({len(logs)}). now analyzing...')
+
+        previous_session = open('record_book.txt', 'r')
+        previous_session_info = ''.join(previous_session.readlines())
+        previous_session.close()
+        tokens = []
+        for log in t_logs:
+            try:
+                value = int.from_bytes(log['data'], byteorder='big')
+                zeroes = str(value).replace('.','').count('0')
+                sender_address = str(w3.to_hex(log['topics'][1]))
+                recipient_address = str(w3.to_hex(log['topics'][2]))
+
+                if is_null(sender_address, 'x') is True \
+                    and is_null(recipient_address, 'x') is False \
+                    and (value / 10**9) >= 10**6 \
+                    and (not log['address'] in tokens) \
+                    and (not log['address'] in previous_session_info):
+                    if recipient_address[:26] == '0x000000000000000000000000' and len(recipient_address) > 60:
+                        recipient_address = f'0x{recipient_address[26:]}'
+
+                    creations.update({f"{log['address']}": [int(log['blockNumber']), log['transactionHash'].hex()]})
+                    tokens.append(log['address'])
+            except:
+                pass
+
+        print(f'created ledger ({len(creations)} subjects). going deeper.')
+        actual_creations = 0
+        if len(creations) > 0:
+            for token_address, tx_info in creations.items():
+                if is_creation_tx(token_address, tx_info):
+                    token_stats = get_contract_wallet_txns(token_address, latest_block, back_stretch)
+                    if len(token_stats) > 0:
+                        print(f'\n--------------------------------\n')
+                        actual_creations += 1
+                        with open('record_book.txt', 'a') as f_w:
+                            f_w.write(f'{token_address}, \n')
+                            f_w.close()
+                        print('Found token with potential')
+                        print(f'Token address: {token_address}')
+                        print(f'Created on block #{tx_info[0]}\n')
+                        for stat in token_stats:
+                            info_to_write = str(stat).replace("'", "")
+                            print(f'{info_to_write}\n')
+                        try:
+                            print('trying to get name and symbol')
+                            abi = [{"inputs":[],
+                            "name":"name",
+                            "outputs":[{
+                                "internalType":"string",
+                                "name":"",
+                                "type":"string"}],
+                            "stateMutability":"view",
+                            "type":"function"},
+                            {"inputs":[],
+                            "name":"symbol",
+                            "outputs":[{
+                                "internalType":"string",
+                                "name":"",
+                                "type":"string"}],
+                            "stateMutability":"view",
+                            "type":"function"}]
+
+                            contract = w3.eth.contract(token_address , abi = abi)
+                            token_name = contract.functions.name().call()
+                            token_symbol = contract.functions.symbol().call()
+
+                            print(f'Name: {token_name}')
+                            print(f'Symbol: {token_symbol}')
+                            for stat in token_stats:
+                                sign = '🚨' if ('png' in stat['image_url']) else '⬛'
+                                await bot.sendMessage(chat_id='@th3k1ll3r', text=f"{sign} {token_symbol} {sign}: {stat['image_url']}\ncurrent token price: {stat['relative_token_price']}\n\nLP info + price chart: https://coinmarketcap.com/dexscan/{chain}/{stat['contract_wallet_address']}\n\nchain explorer: {bscscan_api.replace('api.','').replace('/api/', '')}/token/{token_address}")
+                        except:
+                            print('could not parse name and symbol...')
+                        print(f'\n--------------------------------\n')
+        print(f'Found {actual_creations} potential mooners from {len(creations)} subjects.')
+        absence = time.time()-p_start
+        print(f'Finished search round in {round(absence)} seconds.')
+        knockout = 300
+        if knockout - absence > 0:
+            print(f'Taking a well deserved {round((knockout - absence)/60)}-minute break...')
+            time.sleep(knockout - absence)
+
 def send_req(url, params):
     RETRY = 0
     while (RETRY < 3):
@@ -199,138 +328,19 @@ def get_contract_wallet_txns(token_address, latest_block, back_stretch):
         print(f'Found {len(balance_book)} potential LPs for the token: {token_address}')
     print(f'finished in {round(time.time()-start)} seconds')
     return(balance_book)
-
-async def func():
-    bot = telegram.Bot(os.environ['TELEBOTAPI'])
-    await bot.sendMessage(chat_id='@th3k1ll3r', text=f"code base modified\nrebooted to apply updates")
-    time.sleep(10)
-    await bot.sendMessage(chat_id='@th3k1ll3r', text=f"target chain: {chain.lower()}")
-    time.sleep(2)
-    await bot.sendMessage(chat_id='@th3k1ll3r', text="bravo6\ngoing dark")
-    print("bravo6\ngoing dark")
-    
-    while True:
-        p_start = time.time()
-        latest_block = w3.eth.block_number
-
-        print('\n***************************************')
-        print(f'checking what happened (~{back_stretch/(60*blockspermin)}hrs) ago on {chain.upper()}...')
-        no_of_chunks = round(minutes/5)
-    
-        print(f'took {latest_block} as latest block')
-        hit_wall = False
-        for i in range(no_of_chunks):
-            if back_stretch - width*(i+1) < front_limit:
-                print('hit a wall')
-                hit_wall = True
-                break
-            print(f'parsing chunk {i+1}: { latest_block - (back_stretch - width*i)} to { latest_block - (back_stretch - width*(i+1))}')
-            filter_params = {
-                    'fromBlock': latest_block - (back_stretch - width*i),
-                    'toBlock': latest_block - (back_stretch - width*(i+1)),
-                    'topics': [TRANSFER_EVENT_SIGNATURE]
-                }
-            logs = w3.eth.get_logs(filter_params)
-            print('finished parsing chunk')
-            if i == 0:
-                t_logs = logs
-            elif i > 0:
-                t_logs += logs
-        if hit_wall:
-            time.sleep(60)
-            continue
-        creations = {}
-        print(f'Done parsing logs ({len(logs)}). now analyzing...')
-	    
-        previous_session = open('record_book.txt', 'r')
-        previous_session_info = ''.join(previous_session.readlines())
-        previous_session.close()
-        tokens = []
-        for log in t_logs:
-            try:
-                value = int.from_bytes(log['data'], byteorder='big')
-                zeroes = str(value).replace('.','').count('0')
-                sender_address = str(w3.to_hex(log['topics'][1]))
-                recipient_address = str(w3.to_hex(log['topics'][2]))
-
-                if is_null(sender_address, 'x') is True \
-                    and is_null(recipient_address, 'x') is False \
-                    and (value / 10**9) >= 10**6 \
-                    and (not log['address'] in tokens) \
-                    and (not log['address'] in previous_session_info):
-                    if recipient_address[:26] == '0x000000000000000000000000' and len(recipient_address) > 60:
-                        recipient_address = f'0x{recipient_address[26:]}'
-
-                    creations.update({f"{log['address']}": [int(log['blockNumber']), log['transactionHash'].hex()]})
-                    tokens.append(log['address'])
-
-            except:
-                pass
-
-        print(f'created ledger ({len(creations)} subjects). going deeper.')
-        actual_creations = 0
-        if len(creations) > 0:
-            for token_address, tx_info in creations.items():
-                if is_creation_tx(token_address, tx_info):
-                    token_stats = get_contract_wallet_txns(token_address, latest_block, back_stretch)
-                    if len(token_stats) > 0:
-                        print(f'\n--------------------------------\n')
-                        actual_creations += 1
-                        with open('record_book.txt', 'a') as f_w:
-                            f_w.write(f'{token_address}, \n')
-                            f_w.close()
-                        print('Found token with potential')
-                        print(f'Token address: {token_address}')
-                        print(f'Created on block #{tx_info[0]}\n')
-                        for stat in token_stats:
-                            info_to_write = str(stat).replace("'", "")
-                            print(f'{info_to_write}\n')
-                        try:
-                            print('trying to get name and symbol')
-                            abi = [{"inputs":[],
-                            "name":"name",
-                            "outputs":[{
-                                "internalType":"string",
-                                "name":"",
-                                "type":"string"}],
-                            "stateMutability":"view",
-                            "type":"function"},
-                            {"inputs":[],
-                            "name":"symbol",
-                            "outputs":[{
-                                "internalType":"string",
-                                "name":"",
-                                "type":"string"}],
-                            "stateMutability":"view",
-                            "type":"function"}]
-
-                            contract = w3.eth.contract(token_address , abi = abi)
-                            token_name = contract.functions.name().call()
-                            token_symbol = contract.functions.symbol().call()
-
-                            print(f'Name: {token_name}')
-                            print(f'Symbol: {token_symbol}')
-                            for stat in token_stats:
-                                sign = '🚨' if ('png' in stat['image_url']) else '⬛'
-                                await bot.sendMessage(chat_id='@th3k1ll3r', text=f"{sign} {token_symbol} {sign}: {stat['image_url']}\ncurrent token price: {stat['relative_token_price']}\n\nLP info + price chart: https://coinmarketcap.com/dexscan/{chain}/{stat['contract_wallet_address']}\n\nchain explorer: {bscscan_api.replace('api.','').replace('/api/', '')}/token/{token_address}")
-                        except:
-                            print('could not parse name and symbol...')
-                        print(f'\n--------------------------------\n')
-        print(f'Found {actual_creations} potential mooners from {len(creations)} subjects.')
-        absence = time.time()-p_start
-        print(f'Finished search round in {round(absence)} seconds.')
-        knockout = 300
-        if knockout - absence > 0:
-            print(f'Taking a well deserved {round((knockout - absence)/60)}-minute break...')
-            time.sleep(knockout - absence)
-
+	
 def main():
-    asyncio.run(func())	
+    while True:
+        try:
+            asyncio.run(count())
+        except:
+            print('EXCEPTION')
+            time.sleep(60)
+		
 mainthread = threading.Thread(target=main,)
+mainthread.start()
 
 app = FastAPI()
 @app.get("/")
 async def confirm(request: Request):
-    if not mainthread.is_alive():
-        mainthread.start()
     return """My process is purely logistical, narrowly focused by design. I’m not here to take sides. It’s not my place to formulate any opinion. No one who can afford me needs to waste time winning me to some cause."""
