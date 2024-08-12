09756fa02748b8fe7f56a9398d5372e36c87e919
anon <122219518+avocado-beans@users.noreply.github.com>
2024-08-12 18:14:07 +0200

    msg_con

--- main.py
+++ main.py
@@ -217,7 +217,10 @@ async def func():
             time.sleep(knockout - absence)
 
 def msg_construct(token_address, stat):
-    text = f"current token price: [{stat['relative_token_price'].replace(".", "\\.")}](https://coinmarketcap.com/dexscan/{chain}/{stat['contract_wallet_address']})\n\ntoken address: [{token_address}]({bscscan_api.replace('api.','').replace('/api/', '')}/token/{token_address})\n\nliquidity pool address: [{stat['contract_wallet_address']}]({bscscan_api.replace('api.','').replace('/api/', '')}/token/{stat['contract_wallet_address']})"
+    token_addressHL = f"{bscscan_api.replace('api.','').replace('/api/', '')}/token/{token_address}"
+    lp_addressHL = f"{bscscan_api.replace('api.','').replace('/api/', '')}/token/{stat['contract_wallet_address']}"
+    cmcHL = f"https://coinmarketcap.com/dexscan/{chain}/{stat['contract_wallet_address']}"
+    text = f"current token price: [{stat['relative_token_price'].replace(".", "\\.")}]({cmcHL})\n\ntoken address: [{token_address}]({token_addressHL})\n\nliquidity pool address: [{stat['contract_wallet_address']}]({lp_addressHL})"
     return text
 	
 def lockedburned(lp_address, from_block):
