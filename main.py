25238eeaba67cd42c9bd3846cbd051523e3b75b3
anon <122219518+avocado-beans@users.noreply.github.com>
2024-08-21 13:00:17 +0200

    5min

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ abi = [{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"",
 
 blockspermin = 20
 minutes = 5
-back_stretch_minutes = 30
+back_stretch_minutes = 5
 width = minutes*blockspermin
 back_stretch = back_stretch_minutes*blockspermin
 
